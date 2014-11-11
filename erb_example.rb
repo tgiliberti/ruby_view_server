34f2bab70960270f49ed93aa2f5404821cc621c9
Thiago Scatigno <thiago@icity.com.br>
2014-11-11 21:57:53 -0200

    insercao de pagina html

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,8 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+
+y = ["teste", "ola", "iai"]
+template = ERB.new "<% y.map do |a| puts a.length end %>"
+puts template.result(binding)
